70a074a9c5adbef533585e4182512e124dd4edee
Runar Tobiassen <114917472+runarto@users.noreply.github.com>
2023-12-06 18:31:37 +0100

    hello

--- predictionbot.py
+++ predictionbot.py
@@ -27,11 +27,11 @@ def run_bot():
 
 async def main_bot():
     
-    scheduler.add_job(update_jobs, 'cron', day_of_week='wed', hour=18, minute=25, args=[scheduler], name='update_jobs')
+    scheduler.add_job(update_jobs, 'cron', day_of_week='wed', hour=18, minute=35, args=[scheduler], name='update_jobs')
 
-    scheduler.add_job(send_leaderboard_message, 'cron', day_of_week='wed', hour = 23, minute=30, name='test_run')
+    scheduler.add_job(send_leaderboard_message, 'cron', day_of_week='wed', hour = 23, minute=33, name='test_run')
 
-    scheduler.add_job(send_scheduled_matches, 'cron', day_of_week='wed', hour=18, minute=30, timezone=perms.timezone, name='send_scheduled_matches')
+    scheduler.add_job(send_scheduled_matches, 'cron', day_of_week='wed', hour=18, minute=36, timezone=perms.timezone, name='send_scheduled_matches')
 
     scheduler.start()
 
